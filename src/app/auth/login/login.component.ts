41fdbd9e5edb5841885d11b4d74d442f30bda868
chithambaramchen <nchitha493@gmail.com>
2023-03-23 18:31:16 +0530

    adding dynamic value in template driven form

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -6,17 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  user: User = {
+  user = {
     username: "",
-    password: ""
+    password: "",
+    test:[{item:1},{item:2}]
   };
   submit() {
     console.log(this.user)
     alert(`Submited with: ${this.user.username} ${this.user.password}`);
   }
-
+  addItem(){
+    this.user.test.push({item:0})
+  }
 }
 export interface User {
   username: string;
   password: string;
-}
+  test:number[]}
