f4ec51d67b075fbe159d308cf2fe49d656074090
chithambaramchen <nchitha493@gmail.com>
2023-03-23 21:21:16 +0530

    authGaurd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guard/auth.gaurd';
 
 const routes: Routes = [
   {
@@ -16,6 +17,8 @@ const routes: Routes = [
       import(`./home/home.module`).then(
         (m) => m.HomeModule
       ),
+      canActivate: [AuthGuard]
+      
   }
 ];
 
