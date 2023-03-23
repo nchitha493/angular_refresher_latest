d32bc4cea9bf5facb47bb1d5ffd4cd52cbe0399d
chithambaramchen <nchitha493@gmail.com>
2023-03-23 15:47:54 +0530

    template driven forms

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  user: User = {
+    username: "",
+    password: ""
+  };
+  submit() {
+    console.log(this.user)
+    alert(`Submited with: ${this.user.username} ${this.user.password}`);
+  }
 
+}
+export interface User {
+  username: string;
+  password: string;
 }
